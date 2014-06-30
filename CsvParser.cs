19fd940d2c2c37fd45942090b4ecd30e57cbded9
Jason Crease <jason.crease@red-gate.com>
2014-06-30 18:23:51 +0100

    added parser

---
+++ CsvParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FootballDescent
+{
+    class CsvParser
+    {
+        public Game[] Games { get; private set; }
+        public Player[] Players { get; private set; }
+        private List<Game> gs = new List<Game>();
+        private Dictionary<string, Player> ps = new Dictionary<string, Player>(); 
+
+        public void Go()
+        {
+            StreamReader sr = new StreamReader(".\\..\\..\\Results.csv");
+            string contents = sr.ReadToEnd().Replace("\r", "");
+            sr.Dispose();
+
+            string[] x = contents.Split('\n');
+
+            for (int i = 1; i < 481; i++)
+            {
+                string name = x[i].Split(',')[0];
+                if(!ps.ContainsKey(name))
+                {
+                    ps[name] = new Player(name, 0);
+                    ps[name].GamesPlayed = 1;
+                }
+                else
+                {
+                    ps[name].GamesPlayed++;
+                }
+            }
+
+            for (int i = 0; i < 48; i++ )
+            {
+                Game g = new Game();
+                g.GoalDiff = double.Parse(x[i * 10 + 1].Split(',')[2]);
+
+                for (int a = 0; a < 5; a++)
+                    g.TA[a] = ps[x[i*10 + a + 1].Split(',')[0]];
+                for (int b = 0; b < 5; b++)
+                    g.TB[b] = ps[x[i*10 + b + 6].Split(',')[0]];
+                gs.Add(g);
+            }
+
+            Games = gs.ToArray();
+            Players = ps.Select(z => z.Value).ToArray();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace FootballDescent
     {
         public string Name { get; private set; }
         public double Quality;
+        public int GamesPlayed = 0;
 
         public Player(string name, double quality)
         {
@@ -20,20 +21,9 @@ namespace FootballDescent
 
     class Game
     {
-        public double GoalDiff { get; private set; }
-        public Player[] TA = new Player[3];
-        public Player[] TB = new Player[3];
-
-        public Game(int goalDiff, Player[] players, int a0, int a1, int a2, int b0, int b1, int b2)
-        {
-            GoalDiff = goalDiff;
-            TA[0] = players[a0];
-            TA[1] = players[a1];
-            TA[2] = players[a2];
-            TB[0] = players[b0];
-            TB[1] = players[b1];
-            TB[2] = players[b2];
-        }
+        public double GoalDiff { get; set; }
+        public Player[] TA = new Player[5];
+        public Player[] TB = new Player[5];
     }
 
     class Program
@@ -42,17 +32,16 @@ namespace FootballDescent
 
         static void Main(string[] args)
         {
-            Game[] games;
-            Player[] players;
-            InitGamesAndPlayers(out games, out players);
+            CsvParser parser = new CsvParser();
+            parser.Go();
 
-            for (int goes = 0; goes < 2; goes++)
+            for (int goes = 0; goes < 1; goes++)
             {
-                for (int i = 0; i < 1000000; i++)
-                    GradDown(games);
+                for (int i = 0; i < 10000000; i++)
+                    GradDown(parser.Games);
 
-                foreach(Player p in players.OrderByDescending(x => x.Quality))
-                    Console.WriteLine("{0} {1}", p.Name, p.Quality.ToString("0.000"));
+                foreach (Player p in parser.Players.OrderByDescending(x => x.Quality))
+                    Console.WriteLine("{0, 12} {1}", p.Name, p.Quality.ToString("0.000"));
             }
 
             Console.ReadLine();
@@ -64,51 +53,24 @@ namespace FootballDescent
 
             double pred = 0f;
 
-            for (int i = 0; i < 3; i++)
+            //if (!g.TA.All(x => x.GamesPlayed > 3)) return;
+            //if (!g.TB.All(x => x.GamesPlayed > 3)) return;
+
+            for (int i = 0; i < 5; i++)
             {
                 pred += g.TA[i].Quality;
                 pred -= g.TB[i].Quality;
             }
 
-            double diff = (g.GoalDiff - pred) / 100f;
+            double diff = (g.GoalDiff - pred) / 1000f;
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 5; i++)
             {
-                g.TA[i].Quality += diff;
-                g.TB[i].Quality -= diff;
+                if(g.TA[i].GamesPlayed > 5)
+                    g.TA[i].Quality += diff;
+                if (g.TB[i].GamesPlayed > 5)
+                    g.TB[i].Quality -= diff;
             }
-        }
-
-        private static void InitGamesAndPlayers(out Game[] games, out Player[] p)
-        {
-            p = new Player[10];
-            p[0] = new Player("A", 0);
-            p[1] = new Player("B", 0);
-            p[2] = new Player("C", 0);
-            p[3] = new Player("D", 0);
-            p[4] = new Player("E", 0);
-            p[5] = new Player("F", 0);
-            p[6] = new Player("G", 0);
-            p[7] = new Player("H", 0);
-            p[8] = new Player("I", 0);
-            p[9] = new Player("J", 0);
-
-            games = new Game[15];
-            games[0] = new Game(2, p, 0, 1, 2, 3, 4, 5);
-            games[1] = new Game(1, p, 1, 2, 3, 4, 5, 6);
-            games[2] = new Game(0, p, 2, 3, 4, 5, 6, 7);
-            games[3] = new Game(-1, p, 3, 4, 5, 6, 7, 8);
-            games[4] = new Game(-2, p, 4, 5, 6, 7, 8, 9);
-            games[5] = new Game(2, p, 0, 1, 6, 3, 4, 5);
-            games[6] = new Game(1, p, 1, 2, 3, 4, 5, 6);
-            games[7] = new Game(0, p, 2, 7, 4, 5, 6, 7);
-            games[8] = new Game(-1, p, 9, 2, 5, 6, 7, 8);
-            games[9] = new Game(-2, p, 2, 5, 6, 7, 8, 0);
-            games[10] = new Game(2, p, 9, 8, 2, 6, 4, 5);
-            games[11] = new Game(1, p, 1, 9, 3, 0, 5, 6);
-            games[12] = new Game(0, p, 9, 8, 4, 1, 6, 7);
-            games[13] = new Game(-1, p, 3, 0, 2, 6, 7, 8);
-            games[14] = new Game(-2, p, 4, 5, 1, 7, 2, 9);
         }
     }
 }
